fa60017e544479cee7032a901ce3dc341c9581ac
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-26 13:25:38 -0300

    test

---
+++ huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "heap.h"
+#include "huffman.h"
+
+/**
+ * freq_cmp - compare two frequencies
+ * @a: first
+ * @b: second
+ * Return: difference
+ */
+int freq_cmp(int a, b)
+{
+return (a - b);
+}
+
+/**
+ * create_nested - create binary tree node with data as symbol
+ * @data: symbol data
+ * @freq: symbol frequency
+ * Return: created node
+ */
+binary_tree_node_t* created_nested(char *data, size_t *freq)
+{
+symbol_t *symbol = symbol_create(data, freq);
+if (!symbol)
+{
+return (NULL);
+}
+
+binary_tree_node_t *nested = binary_tree_node(NULL, symbol);
+return (nested);
+}
+
+/**
+ * huffman_priority_queue - creates a priority queue for HF coding
+ * @data: array of characters
+ * @freq: array containing associated frequencies
+ * @size: size of both data and freq arrays
+ * Return: priority queue
+ */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+heap_t *priority_queue;
+if (!data || !freq || !size)
+{
+return (NULL);
+}
+priority_queue = heap_create(freq_cmp);
+if (!priority_queue)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+new_node = create_nested(data[i], freq[i]);
+if (!new_node)
+{
+free(priority_queue);
+return (NULL);
+}
+current_node = heap_insert(priority_queue, new_node);
+}
+
+return (priority_queue);
+}
