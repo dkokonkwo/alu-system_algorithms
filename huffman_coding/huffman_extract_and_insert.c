e2cee2e7e430c3c672106e023800c2f3053f4114
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-27 08:46:53 -0300

    checking 5

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -50,9 +50,9 @@ if (!heap_insert(priority_queue, new_symbol))
 {
 free(new_node);
 free(new_symbol);
+printf("worked\n");
 return (0);
 }
 free(left_symbol), free(right_symbol);
-printf("worked\n");
 return (1);
 }
