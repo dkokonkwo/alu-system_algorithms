ce56d8326e1f1a5717cc23a317b6eacbd4e171dc
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-27 09:47:40 -0300

    new test

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -10,49 +10,50 @@
  */
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
-binary_tree_node_t *new_node;
-symbol_t *new_symbol, *left_symbol, *right_symbol;
+binary_tree_node_t *new_node, *left_node, *right_node;
+symbol_t *new_symbol;
+size_t new_freq;
 if (!priority_queue || priority_queue->size < 2)
 {
+printf("Broke 1");
 return (0);
 }
-left_symbol = (symbol_t *) heap_extract(priority_queue);
-right_symbol = (symbol_t *) heap_extract(priority_queue);
-if (!right_symbol || !left_symbol)
+left_node = (binary_tree_node_t *) heap_extract(priority_queue);
+right_node= (binary_tree_node_t *) heap_extract(priority_queue);
+if (!right_node || !left_node)
 {
+printf("Broke 2");
 return (0);
 }
-new_symbol = symbol_create(-1, (left_symbol->freq + right_symbol->freq));
+new_freq = ((symbol_t *) left_node->data)->freq + ((symbol_t *) right_node->data)->freq;
+printf("New freq: %lu\n", new_freq);
+new_symbol = symbol_create('$', new_freq);
 if (!new_symbol)
 {
-free(right_symbol);
-free(left_symbol);
+printf("Not working\n");
+free(right_node);
+free(left_node);
 return (0);
 }
 new_node = binary_tree_node(NULL, new_symbol);
 if(!new_node)
 {
-free(right_symbol);
-free(left_symbol);
+free(right_node);
+free(left_node);
 free(new_symbol);
+printf("Broke 4");
 return (0);
 }
-new_node->left = binary_tree_node(new_node, left_symbol);
-new_node->right = binary_tree_node(new_node, right_symbol);
-if (!new_node->left || !new_node->right)
-{
-free(right_symbol);
-free(left_symbol);
-free(new_symbol);
-free(new_node);
-}
+new_node->left = left_node;
+new_node->right = right_node;
 if (!heap_insert(priority_queue, new_symbol))
 {
 free(new_node);
 free(new_symbol);
-printf("worked\n");
+printf("Broke 5");
 return (0);
 }
-free(left_symbol), free(right_symbol);
+free(left_node), free(right_node);
 return (1);
 }
+
