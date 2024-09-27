226186c4a0590f07b16d2f98c793d6d6d0ab345c
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-27 09:51:44 -0300

    should work

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -15,22 +15,18 @@ symbol_t *new_symbol;
 size_t new_freq;
 if (!priority_queue || priority_queue->size < 2)
 {
-printf("Broke 1");
 return (0);
 }
 left_node = (binary_tree_node_t *) heap_extract(priority_queue);
 right_node= (binary_tree_node_t *) heap_extract(priority_queue);
 if (!right_node || !left_node)
 {
-printf("Broke 2");
 return (0);
 }
 new_freq = ((symbol_t *) left_node->data)->freq + ((symbol_t *) right_node->data)->freq;
-printf("New freq: %lu\n", new_freq);
-new_symbol = symbol_create('$', new_freq);
+new_symbol = symbol_create(-1, new_freq);
 if (!new_symbol)
 {
-printf("Not working\n");
 free(right_node);
 free(left_node);
 return (0);
@@ -41,16 +37,16 @@ if(!new_node)
 free(right_node);
 free(left_node);
 free(new_symbol);
-printf("Broke 4");
 return (0);
 }
 new_node->left = left_node;
 new_node->right = right_node;
-if (!heap_insert(priority_queue, new_symbol))
+left_node->parent = new_node;
+right_node->parent = new_node;
+if (!heap_insert(priority_queue, new_node))
 {
 free(new_node);
 free(new_symbol);
-printf("Broke 5");
 return (0);
 }
 free(left_node), free(right_node);
