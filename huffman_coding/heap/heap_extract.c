405da4011b41ede015d29fe84b8df0a84a90d061
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-27 13:09:08 -0300

    new logic

--- huffman_coding/heap/heap_extract.c
+++ huffman_coding/heap/heap_extract.c
@@ -24,10 +24,10 @@ binary_tree_node_t *smallest;
 while (node->left)
 {
 smallest = node->left;
-if (node->right && heap->data_cmp(node->right->data, node->left->data) < 0)
+if (node->right && heap->data_cmp(node->right->data, node->left->data) <= 0)
 smallest = node->right;
 
-if (heap->data_cmp(smallest->data, node->data) < 0)
+if (heap->data_cmp(smallest->data, node->data) <= 0)
 {
 swap(node, smallest);
 node = smallest;
